a8d66d7f65e9cf1450615da173f772ff8b7ec9f1
Mauricio Klein <mauricio.klein@fromatob.com>
2018-01-10 00:41:09 +0100

    Adjust main method to provide Redis client to Router constructor

--- cmd/go-url-shortner/main.go
+++ cmd/go-url-shortner/main.go
@@ -8,34 +8,51 @@ import (
 	"os"
 
 	// Postgres connector
+	"github.com/go-redis/redis"
 	_ "github.com/lib/pq"
 	urlshortner "github.com/mauricioklein/go-url-shortner"
 	"github.com/mauricioklein/go-url-shortner/store"
 )
 
 var (
+	// Postgres errors
 	errNoDbHost     = errors.New("no postgres host provided")
 	errNoDbUser     = errors.New("no postgres user provided")
 	errNoDbDatabase = errors.New("no postgres database provided")
+
+	// Redis errors
+	errNoRedisHost = errors.New("no redis host provided")
+	errNoRedisPort = errors.New("no redis port provided")
 )
 
 func main() {
-	host := os.Getenv("DB_HOST")
-	if host == "" {
+	pgHost := os.Getenv("DB_HOST")
+	if pgHost == "" {
 		panic(errNoDbHost)
 	}
 
-	user := os.Getenv("DB_USER")
-	if user == "" {
+	pgUser := os.Getenv("DB_USER")
+	if pgUser == "" {
 		panic(errNoDbUser)
 	}
 
-	database := os.Getenv("DB_DATABASE")
-	if database == "" {
+	pgDb := os.Getenv("DB_DATABASE")
+	if pgDb == "" {
 		panic(errNoDbDatabase)
 	}
 
-	dbinfo := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", host, user, database)
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		panic(errNoRedisHost)
+	}
+
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		panic(errNoRedisPort)
+	}
+
+	// establish connection to Postgres
+	dbinfo := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable", pgHost, pgUser, pgDb)
 	db, err := sql.Open("postgres", dbinfo)
 	if err != nil {
 		panic(err)
@@ -48,6 +65,21 @@ func main() {
 
 	linkStore := store.NewLinkStore(db)
 
-	muxRouter := urlshortner.NewRouter(linkStore)
+	// establish connection to Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
+		Password: "",
+		DB:       0,
+	})
+
+	_, err = redisClient.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	redisStore := store.NewRedisStore(redisClient)
+	muxRouter := urlshortner.NewRouter(linkStore, redisStore)
+
+	fmt.Printf("Now serving on port :8080")
 	http.ListenAndServe(":8080", muxRouter)
 }
